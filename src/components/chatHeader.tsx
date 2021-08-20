b04cf8b625900e2eea509e9b7a7df3d5d2304b16
Francisco Briones <francisco.briones.l@gmail.com>
2021-08-19 22:12:35 -0400

    use material toolbar

--- src/components/chatHeader.tsx
+++ src/components/chatHeader.tsx
@@ -1,8 +1,15 @@
+import { Typography } from "@material-ui/core";
+import { AppBar, Toolbar } from "@material-ui/core";
+
 function ChatHeader() {
   return (
-    <div className="chat-header">
-      Automated Questions Chat
-    </div>
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h6" color="inherit">
+          Automated profile definition
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
